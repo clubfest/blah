f1bb6def6bcd669af5ce0cb959877a1b0a7623a5
Chor <cl676@cornell.edu>
2014-07-11 19:54:51 -0400

    with router

---
+++ routes.js
@@ -0,0 +1,12 @@
+
+Router.configure({
+  // layoutTemplate: 'layout',
+  notFoundTemplate: 'notFound',
+  // loadingTemplate: 'loading',
+});
+
+Router.map(function() {
+  this.route('home', {
+    path: '/',
+  });
+});
